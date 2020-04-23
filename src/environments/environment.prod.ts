d8ff331d15f6e661006c7b12f2dd50e96bb7d2e5
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-04-23 11:51:28 -0400

    google login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDUTBn-IBOa1krBdtOVXT5RkDL_F8OtcLw",
+    authDomain: "oshop-c79f6.firebaseapp.com",
+    databaseURL: "https://oshop-c79f6.firebaseio.com",
+    projectId: "oshop-c79f6",
+    storageBucket: "oshop-c79f6.appspot.com",
+    messagingSenderId: "1066018021905",
+    appId: "1:1066018021905:web:ae330fca288edb725c07b0",
+    measurementId: "G-Q23TP9WD7R"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDUTBn-IBOa1krBdtOVXT5RkDL_F8OtcLw",
+    authDomain: "oshop-c79f6.firebaseapp.com",
+    databaseURL: "https://oshop-c79f6.firebaseio.com",
+    projectId: "oshop-c79f6",
+    storageBucket: "oshop-c79f6.appspot.com",
+    messagingSenderId: "1066018021905",
+    appId: "1:1066018021905:web:ae330fca288edb725c07b0",
+    measurementId: "G-Q23TP9WD7R"
+  }
 };
 
 /*
